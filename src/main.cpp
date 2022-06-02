bfbffe55e3f0bc5223c6ba6f8c6563cc7257f0d8
AdaSalvadorAvalos <100204789+AdaSalvadorAvalos@users.noreply.github.com>
2022-06-02 09:44:27 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -55,23 +55,17 @@ void toggleLED(void * parameter){
 
 
 void uploadToAWS(void * parameter){
-    // Implement your custom logic here
     Serial.print("Task is running on: ");
   Serial.println(xPortGetCoreID());
 
-    // Turn the LED off
-    // Turn the LED off
     digitalWrite(led2, HIGH);
-    // Pause the task again for 500ms
     vTaskDelay(500 / portTICK_PERIOD_MS);
 
     digitalWrite(led2, LOW);
     
-    // Pause the task again for 500ms
     vTaskDelay(500 / portTICK_PERIOD_MS);
     
 
-    // When you're done, call vTaskDelete. Don't forget this!
     vTaskDelete(NULL);
 }
 
